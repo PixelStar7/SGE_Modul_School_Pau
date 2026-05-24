0ceeea80e3e5bd50b58add5563f420a8b58d41ec
Pau Tort Balsach <pautortbalsach@gmail.com>
2026-05-24 12:27:21 +0200

    Versió v24.0

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'School',
-    'version': '23.0',
+    'version': '24.0',
     'category': 'Education',
     'summary': 'School Management',
     'description': """
